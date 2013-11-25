c32a8f5df1dbc81358f67cfe563e23433b55f3da
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-11-25 10:15:56 -0600

    Initial commit

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Node pet project'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '51670716cfa3df6bde6d2d1ff34aa3900e0c87e2',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
